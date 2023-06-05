cba83fdbd50d06710f4d2f29bcb060700d5f6dd4
Roman <yakubovroma5@gmail.com>
2023-06-05 15:27:04 +0300

    the api was updated

--- src/congif.js
+++ src/congif.js
@@ -1,5 +1,5 @@
 import { Dropbox } from "dropbox";
 
-const dropboxToken = 'sl.Bfhakc6fR1T5aXYQnDok_VTEchPlUOGOGd8Ywxt5ZUeH-cTNS8mtNzZTtjPaIWXXsHRdCbJHiPR_ABSHh_M9ho23yoFb_et54C5XP9_DPIteXXO_GWZ_I9ysz9UbtNdt35Oqnz1PH-A';
+const dropboxToken = 'sl.Bfs0-KQf4cQC2rZxksjjAGnSxh5uI9Vy1piPQe1_a8CPs7fv1s6Yrh-Q4YgPq0ug5oUOV13G70n5oyBxFJhgKjaxHZnUGxXFjA7ArB0bjfokf7MLXqB5zAs_VKD2pUY-ZzAu9iHHBak';
 
 export const dropbox = new Dropbox({ accessToken: dropboxToken });
